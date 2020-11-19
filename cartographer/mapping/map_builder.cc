a981ea2ca428fa8054ecf55b1ad3f18661f383c3
Jason <zhangym127@163.com>
2020-11-19 23:45:52 +0800

    更新了注释

--- cartographer/mapping/map_builder.cc
+++ cartographer/mapping/map_builder.cc
@@ -102,7 +102,7 @@ MapBuilder::MapBuilder(const proto::MapBuilderOptions& options)
 }
 
 /**
- * @brief 添加轨迹构建器
+ * @brief 添加轨迹构建器，是CartoGrapher中最重要的一个函数
  * @param expected_sensor_ids 
  * @param trajectory_options 轨迹配置项
  * @param local_slam_result_callback
@@ -202,71 +202,112 @@ int MapBuilder::AddTrajectoryBuilder(
   return trajectory_id;
 }
 
+/**
+ * @brief 从序列化的数据中构造一个轨迹
+ * @param options_with_sensor_ids_proto
+ * @return 新的轨迹构建器id 
+ */
 int MapBuilder::AddTrajectoryForDeserialization(
     const proto::TrajectoryBuilderOptionsWithSensorIds&
         options_with_sensor_ids_proto) {
+  /** 分配一个新的轨迹ID */
   const int trajectory_id = trajectory_builders_.size();
+  /** 为新的轨迹在轨迹构建器数组上分配空间，暂未初始化 */
   trajectory_builders_.emplace_back();
+  /** 将传感器配置项压入轨迹配置项数组 */
   all_trajectory_builder_options_.push_back(options_with_sensor_ids_proto);
+  /** 确认轨迹构建器数组和其配置项数组长度一致 */
   CHECK_EQ(trajectory_builders_.size(), all_trajectory_builder_options_.size());
+  /** 返回新的轨迹id */
   return trajectory_id;
 }
 
+/**
+ * @brief 完成轨迹
+ * @param trajectory_id 轨迹id
+ */
 void MapBuilder::FinishTrajectory(const int trajectory_id) {
+  /** 断开传感器与该轨迹的关系 */
   sensor_collator_->FinishTrajectory(trajectory_id);
+  /** 在PoseGraph中关闭轨迹 */
   pose_graph_->FinishTrajectory(trajectory_id);
 }
 
+/**
+ * @brief 根据指定的id查询submap，把查询的结果放到response中
+ * @param trajectory_id submap的id
+ * @param response 查询的结果
+ */
 std::string MapBuilder::SubmapToProto(
     const SubmapId& submap_id, proto::SubmapQuery::Response* const response) {
+  /** 检查map的合法性 */
   if (submap_id.trajectory_id < 0 ||
       submap_id.trajectory_id >= num_trajectory_builders()) {
     return "Requested submap from trajectory " +
            std::to_string(submap_id.trajectory_id) + " but there are only " +
            std::to_string(num_trajectory_builders()) + " trajectories.";
   }
-
+  /** 从PoseGraph中查询到submap的数据，并检查数据的有效性 */
   const auto submap_data = pose_graph_->GetSubmapData(submap_id);
   if (submap_data.submap == nullptr) {
     return "Requested submap " + std::to_string(submap_id.submap_index) +
            " from trajectory " + std::to_string(submap_id.trajectory_id) +
            " but it does not exist: maybe it has been trimmed.";
   }
+  /** 将查询到的数据保存到response中 */
   submap_data.submap->ToResponseProto(submap_data.pose, response);
   return "";
 }
 
+/**
+ * @brief 序列化（保存）所有状态数据
+ * @param writer 输出流的指针
+ */
 void MapBuilder::SerializeState(io::ProtoStreamWriterInterface* const writer) {
   io::WritePbStream(*pose_graph_, all_trajectory_builder_options_, writer);
 }
 
+/**
+ * @brief 反序列化（加载）所有状态数据
+ * @param reader 输入流的指针
+ * @param load_frozen_state 是否加载为frozen状态
+ */
 void MapBuilder::LoadState(io::ProtoStreamReaderInterface* const reader,
                            bool load_frozen_state) {
+  /** 创建并初始化反序列化器 */
   io::ProtoStreamDeserializer deserializer(reader);
 
+  /** 反序列化PoseGraph */
   // Create a copy of the pose_graph_proto, such that we can re-write the
   // trajectory ids.
   proto::PoseGraph pose_graph_proto = deserializer.pose_graph();
+  /** 反序列化轨迹构建器选项 */
   const auto& all_builder_options_proto =
       deserializer.all_trajectory_builder_options();
 
+  /** 逐一反序列化每个轨迹 */
   std::map<int, int> trajectory_remapping;
   for (auto& trajectory_proto : *pose_graph_proto.mutable_trajectory()) {
     const auto& options_with_sensor_ids_proto =
         all_builder_options_proto.options_with_sensor_ids(
             trajectory_proto.trajectory_id());
+	/** 添加轨迹 */
     const int new_trajectory_id =
         AddTrajectoryForDeserialization(options_with_sensor_ids_proto);
+	/** 添加轨迹到trajectory_remapping中 */
     CHECK(trajectory_remapping
               .emplace(trajectory_proto.trajectory_id(), new_trajectory_id)
               .second)
         << "Duplicate trajectory ID: " << trajectory_proto.trajectory_id();
+	/** 分配轨迹的id */
     trajectory_proto.set_trajectory_id(new_trajectory_id);
+	/** 轨迹加载为冻结状态 */
     if (load_frozen_state) {
       pose_graph_->FreezeTrajectory(new_trajectory_id);
     }
   }
 
+  //FIXME：这个地方没看懂
   // Apply the calculated remapping to constraints in the pose graph proto.
   for (auto& constraint_proto : *pose_graph_proto.mutable_constraint()) {
     constraint_proto.mutable_submap_id()->set_trajectory_id(
@@ -275,6 +316,7 @@ void MapBuilder::LoadState(io::ProtoStreamReaderInterface* const reader,
         trajectory_remapping.at(constraint_proto.node_id().trajectory_id()));
   }
 
+  /** 恢复每个轨迹的submap */
   MapById<SubmapId, transform::Rigid3d> submap_poses;
   for (const proto::Trajectory& trajectory_proto :
        pose_graph_proto.trajectory()) {
@@ -286,6 +328,7 @@ void MapBuilder::LoadState(io::ProtoStreamReaderInterface* const reader,
     }
   }
 
+  /** 恢复节点的pose */
   MapById<NodeId, transform::Rigid3d> node_poses;
   for (const proto::Trajectory& trajectory_proto :
        pose_graph_proto.trajectory()) {
@@ -296,12 +339,14 @@ void MapBuilder::LoadState(io::ProtoStreamReaderInterface* const reader,
     }
   }
 
+  /** 恢复地标的位姿 */
   // Set global poses of landmarks.
   for (const auto& landmark : pose_graph_proto.landmark_poses()) {
     pose_graph_->SetLandmarkPose(landmark.landmark_id(),
                                  transform::ToRigid3(landmark.global_pose()));
   }
 
+  /** 从输入流中反序列化各种对象 */
   SerializedData proto;
   while (deserializer.ReadNextSerializedData(&proto)) {
     switch (proto.data_case()) {
@@ -375,6 +420,7 @@ void MapBuilder::LoadState(io::ProtoStreamReaderInterface* const reader,
     }
   }
 
+  /** 如果是加载冻结的轨迹 */
   if (load_frozen_state) {
     // Add information about which nodes belong to which submap.
     // Required for 3D pure localization.
